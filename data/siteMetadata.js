47cc03781b85c50d0209cc628b03b2c079b04541
alex <enna@mailbox.org>
2023-11-20 10:53:21 +0100

    Revert "Basic init"
    This reverts commit c4df6c3f425c0dc17bab03ba65188f93a3c69996.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Alexander Weinmann',
-  headerTitle: 'Alex\' Blog',
+  author: 'Tails Azimuth',
+  headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
