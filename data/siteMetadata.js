ed0589b624d6934476ebfad3033faf4e663f081f
alex <enna@mailbox.org>
2023-11-20 10:53:21 +0100

    Revert "Remove yarn.lock"
    This reverts commit 44a7b043696cf5164ee3848e15dbf0bf6b56bcba.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Alex\' Blog',
+  title: 'Next.js Starter Blog',
   author: 'Alexander Weinmann',
   headerTitle: 'Alex\' Blog',
   description: 'A blog created with Next.js and Tailwind.css',
