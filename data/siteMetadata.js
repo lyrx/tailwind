44a7b043696cf5164ee3848e15dbf0bf6b56bcba
alex <enna@mailbox.org>
2023-11-20 09:50:18 +0100

    Remove yarn.lock

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Alex\' Blog',
   author: 'Alexander Weinmann',
   headerTitle: 'Alex\' Blog',
   description: 'A blog created with Next.js and Tailwind.css',
