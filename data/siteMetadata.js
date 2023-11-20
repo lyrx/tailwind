a0c3ce0989b34087ff83aa95c57ed5137a813694
alex <enna@mailbox.org>
2023-11-20 12:06:13 +0100

    Start content replacements

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Alex\' Blog',
   author: 'Alexander Weinmann',
   headerTitle: 'Alex\' Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'My Journey as an IT-Consultant',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
