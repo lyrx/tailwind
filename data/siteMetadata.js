99b127bddd3fc2fa31a4ea0828754224c7c3405d
alex <enna@mailbox.org>
2023-11-20 16:09:46 +0100

    Links, logos, accounts

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,9 +6,9 @@ const siteMetadata = {
   description: 'My Journey as IT-Consultant',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/IchGezeichnet2.png',
+  siteUrl: 'https://www.alexanderweinmann.com/',
+  siteRepo: 'https://github.com/lyrx/tailwind',
+  siteLogo: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'enna@mailbox.org',
