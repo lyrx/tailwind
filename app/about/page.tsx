3e45f651de1942a17632bda5d0a0868d15a7dd66
alex <enna@mailbox.org>
2023-11-29 09:26:25 +0100

    Support for multiple authors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,16 +4,26 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'Authors' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  const defaultAuthor = allAuthors.find((p) => p.slug === 'default') as Authors
+  const defaultContent = coreContent(defaultAuthor)
+  const sparrowHawk = allAuthors.find((p) => p.slug === 'sparrowhawk') as Authors
+  const sparrowHawkContent = coreContent(sparrowHawk)
 
   return (
     <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Authors
+        </h1>
+      </div>
+      <AuthorLayout content={defaultContent}>
+        <MDXLayoutRenderer code={defaultAuthor.body.code} />
+      </AuthorLayout>
+      <AuthorLayout content={sparrowHawkContent}>
+        <MDXLayoutRenderer code={sparrowHawk.body.code} />
       </AuthorLayout>
     </>
   )
