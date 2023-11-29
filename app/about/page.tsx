54b6efbb6d71b581122a657f75d0c9eae3de5cf4
alex <enna@mailbox.org>
2023-11-29 11:19:37 +0100

    Revise Authors page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,13 +11,15 @@ export default function Page() {
   const defaultContent = coreContent(defaultAuthor)
   const sparrowHawk = allAuthors.find((p) => p.slug === 'sparrowhawk') as Authors
   const sparrowHawkContent = coreContent(sparrowHawk)
-
+  const h2Classes =
+    'text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-3xl md:leading-14'
   return (
     <>
       <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Authors
-        </h1>
+
+        <h2 className={h2Classes}>New Author?</h2>
+        <p>This is a blog for nerds, written by nerds. </p>
+        <h2 className={h2Classes}>Active Authors</h2>
       </div>
       <AuthorLayout content={defaultContent}>
         <MDXLayoutRenderer code={defaultAuthor.body.code} />
