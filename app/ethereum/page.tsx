1dabcd0feb5fa3f56aab9969ec8e0a6432c0ba74
alex <enna@mailbox.org>
2023-11-30 13:48:53 +0100

    Ethereum context again

--- app/ethereum/page.tsx
+++ app/ethereum/page.tsx
@@ -9,25 +9,15 @@ const MyComponent: React.FC = () => {
     // Ensure ethereum is available
     // @ts-ignore
     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-      const checkEthereum = () => {
-        // Set ethereum
-        // @ts-ignore
-        setEthereum(window.ethereum)
-      }
-
-      // Add event listener
-      window.addEventListener('resize', checkEthereum)
-
-      // Call the handler right away so state gets updated with initial window size
-      checkEthereum()
-      // Remove event listener on cleanup
-      return () => window.removeEventListener('resize', checkEthereum)
+      // Set ethereum
+      // @ts-ignore
+      setEthereum(window.ethereum)
     }
   }, []) // Empty array ensures that effect runs only on mount
   // @ts-ignore
   return (
     <div>
-      <p>Window ethereum: {ethereum ? 'Ethereum' : 'not set'}</p>
+      <p>Window ethereum: {ethereum ? 'Ethereum found' : 'Ethereum not found'}</p>
     </div>
   )
 }
