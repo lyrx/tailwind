b84d7ff496ce282a8dd71b45ed41379ee6935c3a
alex <enna@mailbox.org>
2023-12-01 09:11:51 +0100

    About-Page changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,14 +16,11 @@ export default function Page() {
     <>
       <div className="space-y-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Authors
+          About
         </h1>
       </div>
       <AuthorLayout content={defaultContent}>
         <MDXLayoutRenderer code={defaultAuthor.body.code} />
-      </AuthorLayout>
-      <AuthorLayout content={sparrowHawkContent}>
-        <MDXLayoutRenderer code={sparrowHawk.body.code} />
       </AuthorLayout>
     </>
   )
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'Authors' },
+  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
