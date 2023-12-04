13f6b3edaaea54e9958e3b5c6c75fc9cdb196ea7
alex <enna@mailbox.org>
2023-12-04 12:37:09 +0100

    Simplify Status-Pasge for Ethers.

--- app/ethereum/page.tsx
+++ app/ethereum/page.tsx
@@ -5,39 +5,20 @@ import Context from '../context/Context'
 import CurrentNetwork from '@/components/ethereum/CurrentNetwork'
 import CurrentSignerAddress from '@/components/ethereum/CurrentSignerAddress'
 import CurrentSignerBalance from '@/components/ethereum/CurrentSignerBalance'
-import { BigNumberish, ethers } from 'ethers'
 
 const EthComponent: React.FC = () => {
-  const [web3Provider, setWeb3Provider] = useState<ethers.Provider | null | undefined>(null)
-  const [signer, setSigner] = useState<ethers.Signer | null | undefined>(null)
-  const [address, setAddress] = useState<string | null | undefined>(null)
-  const [balance, setBalance] = useState<BigNumberish>(0)
-  const [network, setNetwork] = useState<ethers.Network | null>(null)
   const [isInitialized, setIsInitialized] = useState<boolean>(false)
   const context = useContext(Context)
   useEffect(() => {
-    const aprovider = context?.ethersProvider?.web3Provider
-    setWeb3Provider(aprovider)
-    aprovider?.getSigner().then((s) => {
-      setSigner(s)
-      s.getAddress().then((a) => {
-        setAddress(a)
-        aprovider?.getBalance(a).then((b) => {
-          setBalance(b)
-          aprovider?.getNetwork().then((n) => {
-            setNetwork(n)
-            setIsInitialized(true)
-          })
-        })
-      })
-    })
+    setIsInitialized(true)
   }, [context?.ethersProvider?.web3Provider])
 
   const whenInitialized = () => (
     <div>
       {' '}
       <p>
-        Address: <CurrentSignerAddress />:  ETH <CurrentSignerBalance /> (<CurrentNetwork />)-Network
+        Address: <CurrentSignerAddress />: ETH <CurrentSignerBalance /> (<CurrentNetwork />
+        -Network)
       </p>
     </div>
   )
