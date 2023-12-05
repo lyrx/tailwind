55d0181217440f845c77635511a4cead88201411
alex <enna@mailbox.org>
2023-12-05 18:12:10 +0100

    Article on MetaMask-Integration

--- app/ethereum/page.tsx
+++ app/ethereum/page.tsx
@@ -1,33 +1,42 @@
 'use client'
 
-import React, { useEffect, useState, useContext } from 'react'
-import Context from '../context/Context'
 import CurrentNetwork from '@/components/ethereum/CurrentNetwork'
 import CurrentSignerAddress from '@/components/ethereum/CurrentSignerAddress'
 import CurrentSignerBalance from '@/components/ethereum/CurrentSignerBalance'
+import SectionContainer from '@/components/SectionContainer'
 
-const EthComponent: React.FC = () => {
-  const [isInitialized, setIsInitialized] = useState<boolean>(false)
-  const context = useContext(Context)
-  useEffect(() => {
-    setIsInitialized(true)
-  }, [context?.ethersProvider?.web3Provider])
-
-  const whenInitialized = () => (
-    <div>
-      {' '}
-      <p>
-        Address: <CurrentSignerAddress />: ETH <CurrentSignerBalance /> (<CurrentNetwork />
-        -Network)
-      </p>
-    </div>
-  )
-  const whenNotInitialized = () => (
-    <div>
-      <p>'Initializing'</p>
-    </div>
-  )
-  return isInitialized ? whenInitialized() : whenNotInitialized()
-}
+const EthComponent: React.FC = () => (
+  <SectionContainer>
+    <article>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+        <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">
+          <h1>Header</h1>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th>Signer Address</th>
+                <th>Signer Balance (ETH)</th>
+                <th>Network</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <CurrentSignerAddress />
+                </td>
+                <td>
+                  <CurrentSignerBalance />
+                </td>
+                <td>
+                  <CurrentNetwork />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </article>
+  </SectionContainer>
+)
 
 export default EthComponent
