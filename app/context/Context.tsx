3326e50770338832add1e20d3009d8cc8392b9ec
alex <enna@mailbox.org>
2023-12-06 14:18:17 +0100

    Adding default provider

--- app/context/Context.tsx
+++ app/context/Context.tsx
@@ -5,6 +5,7 @@ import { ethers } from 'ethers'
 
 export interface EthersProvider {
   web3Provider: ethers.BrowserProvider | null
+  defaultMainNetProvider: ethers.Provider
 }
 
 export interface ContextType {
@@ -12,6 +13,7 @@ export interface ContextType {
 }
 
 export function createDefaultState() {
+  const ad = ethers.getDefaultProvider('mainnet')
   // @ts-ignore
   if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     console.log(`Browser provider initialized.`)
@@ -19,9 +21,16 @@ export function createDefaultState() {
       ethersProvider: {
         // @ts-ignore
         web3Provider: new ethers.BrowserProvider(window.ethereum),
+        defaultMainNetProvider: ad,
       },
     }
-  } else return { ethersProvider: { web3Provider: null } }
+  } else
+    return {
+      ethersProvider: {
+        web3Provider: null,
+        defaultMainNetProvider: ad,
+      },
+    }
 }
 
 const Context = createContext<ContextType>(createDefaultState())
