932c600c85b8d27461fb5405db450dd297638c7e
alex <enna@mailbox.org>
2023-12-06 18:48:17 +0100

    Revert "Add accounts changed listener to CurrentSignerAddress"
    This reverts commit 2b54380649b0ba6269edcf87a4cdd5fb6019150d.

--- components/ethereum/CurrentSignerBalance.tsx
+++ components/ethereum/CurrentSignerBalance.tsx
@@ -9,31 +9,19 @@ const CurrentSignerAddress: React.FC = () => {
   const context = useContext(Context)
   const [balance, setBalance] = useState<BigNumberish>(0)
   useEffect(() => {
-    const handleSignerAccount = () => {
-      console.log("handleSignerAccount")
-      context.ethersProvider?.web3Provider?.getSigner().then((s) => {
-        return s.getAddress().then((a) => {
-          const address = a
-          const fetchBalance = () => {
-            context.ethersProvider?.web3Provider?.getBalance(address).then((n) => setBalance(n))
-          }
-          fetchBalance()
-
-          const interval = setInterval(fetchBalance, 12000)
-          return () => clearInterval(interval)
-        })
+    context.ethersProvider?.web3Provider?.getSigner().then((s) => {
+      return s.getAddress().then((a) => {
+        const address = a
+        const fetchBalance = () => {
+          context.ethersProvider?.web3Provider?.getBalance(address).then((n) => setBalance(n))
+        }
+        fetchBalance()
+        const interval = setInterval(fetchBalance, 12000)
+        return () => clearInterval(interval)
       })
-    }
-    handleSignerAccount()
-    // @ts-ignore
-    window.ethereum?.on('accountsChanged', handleSignerAccount)
+    })
     // Clean up the interval when the component unmounts
-    return () => {
-      // @ts-ignore
-      window.ethereum?.removeListener('accountsChanged', handleSignerAccount)
-    }
   }, [context.ethersProvider?.web3Provider])
-
   return ethers.formatEther(balance)
 }
 
