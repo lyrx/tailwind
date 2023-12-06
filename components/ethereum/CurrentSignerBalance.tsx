03e1eea05e71eaf6ef6e1af3e69f6fb2fbb964fc
alex <enna@mailbox.org>
2023-12-06 15:52:59 +0100

    Improve display signer balance

--- components/ethereum/CurrentSignerBalance.tsx
+++ components/ethereum/CurrentSignerBalance.tsx
@@ -3,17 +3,25 @@
 import React, { useEffect, useState, useContext } from 'react'
 import Context from '../../app/context/Context'
 import { BigNumberish, ethers } from 'ethers'
+import BalanceMainNet from '@/components/ethereum/BalanceMainNet'
 
 const CurrentSignerAddress: React.FC = () => {
   const context = useContext(Context)
   const [balance, setBalance] = useState<BigNumberish>(0)
   useEffect(() => {
-    context?.ethersProvider?.web3Provider?.getSigner().then((s) => {
+    context.ethersProvider?.web3Provider?.getSigner().then((s) => {
       return s.getAddress().then((a) => {
-        context.ethersProvider?.web3Provider?.getBalance(a).then((b) => setBalance(b))
+        const address = a
+        const fetchBalance = () => {
+          context.ethersProvider?.web3Provider?.getBalance(address).then((n) => setBalance(n))
+        }
+        fetchBalance()
+        const interval = setInterval(fetchBalance, 12000)
+        return () => clearInterval(interval)
       })
     })
-  }, [context?.ethersProvider?.web3Provider])
+    // Clean up the interval when the component unmounts
+  }, [context.ethersProvider?.web3Provider])
   return ethers.formatEther(balance)
 }
 
