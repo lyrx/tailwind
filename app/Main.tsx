35c57f191912b73fe51bac214643ccddded74691
alex <enna@mailbox.org>
2023-12-06 11:56:24 +0100

    Remove current signer address from bottom

--- app/Main.tsx
+++ app/Main.tsx
@@ -83,9 +83,7 @@ export default function Home({ posts }) {
       )}
       {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
-          <span>
-            <CurrentSignerAddress />
-          </span>
+          <span></span>
         </div>
       )}
     </>
