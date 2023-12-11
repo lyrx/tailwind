db09f6fd5ca2c73188fd8df32c8b6661c643239b
alex <enna@mailbox.org>
2023-12-11 17:03:39 +0100

    New component for tokens

--- app/ethereum/page.tsx
+++ app/ethereum/page.tsx
@@ -15,6 +15,7 @@ import ContextProvider from '../context/ContextProvider'
 import { BlockOrNull, BlockSetterOrNull } from '@/components/ethereum/EthersDerivedTypes'
 import Watch from '@/components/ethereum/Watch'
 import LastBlockNumber from '@/components/ethereum/LastBlockNumber'
+import Tokens from '@/components/ethereum/ERC20/Tokens'
 // @ts-ignore
 const EthComponent: React.FC = () => {
   const context = useContext(Context)
@@ -74,33 +75,7 @@ const EthComponent: React.FC = () => {
                   </tr>
                 </tbody>
               </table>
-              <table className="table-auto">
-                <thead>
-                  <tr>
-                    <th>ERC20 Token</th>
-                    <th>Signer Balance</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {[
-                    '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-                    '0x514910771AF9Ca656af840dff83E8264EcF986CA',
-                    '0xB8c77482e45F1F44dE1745F52C74426C631bDD52',
-                    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-                    '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
-                    '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
-                  ].map((a) => (
-                    <tr key={a}>
-                      <td>
-                        <NameOf contractAddress={a} />
-                      </td>
-                      <td>
-                        <SignerBalance contractAddress={a} />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            <Tokens  />
             </div>
           </div>
         </article>
---
+++ components/ethereum/ERC20/Tokens.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React, { useEffect, useState, useContext } from 'react'
+import Context from '../../../app/context/Context'
+import ERC20 from './ERC20'
+import { ethers } from 'ethers'
+import NameOf from '@/components/ethereum/ERC20/NameOf'
+import SignerBalance from '@/components/ethereum/ERC20/SignerBalance'
+
+const Tokens: React.FC = () => {
+  const context = useContext(Context)
+  const [tokens, setTokens] = useState<string[]>([])
+  useEffect(() => {
+    context?.ethersProvider?.web3Provider?.getNetwork().then((n) => {
+      const netWorkName = n.name
+      if (netWorkName === 'mainnet') {
+        setTokens([
+          '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+          '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+          '0xB8c77482e45F1F44dE1745F52C74426C631bDD52',
+          '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+          '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+          '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
+        ])
+      }
+    })
+  }, [context?.ethersProvider?.web3Provider])
+  return tokens.length > 0 ? (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th>ERC20 Token</th>
+          <th>Signer Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokens.map((a) => (
+          <tr key={a}>
+            <td>
+              <NameOf contractAddress={a} />
+            </td>
+            <td>
+              <SignerBalance contractAddress={a} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : null
+}
+
+export default Tokens
